4a536321b12976de8116aba9ea7ec6eaaa2067b9
sakther17 <140296172+sakther17@users.noreply.github.com>
2023-08-09 19:33:17 -0400

    Add files via upload

---
+++ ResellingBook/public_html/script.js
@@ -0,0 +1,17 @@
+
+
+
+document.getElementById('buyButton').addEventListener('click', function () {
+    // Assuming you have a payment page named "payment.html," redirect the user to that page.
+    window.location.href = 'payment.html';
+});
+
+document.getElementById('viewMore').addEventListener('click', function () {
+    // Assuming you have a payment page named "payment.html," redirect the user to that page.
+    window.location.href = 'viewmore.html';
+});
+
+document.getElementById('back').addEventListener('click', function () {
+    // Assuming you have a payment page named "payment.html," redirect the user to that page.
+    window.location.href = 'index.html';
+});
